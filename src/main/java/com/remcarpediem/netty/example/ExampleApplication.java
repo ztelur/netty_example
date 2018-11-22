03bc980e45589c374ef27241cec496eddfd0aa25
Randy <ztelur@gmail.com>
2018-11-22 17:31:16 +0800

    update

--- src/main/java/com/remcarpediem/netty/example/ExampleApplication.java
+++ src/main/java/com/remcarpediem/netty/example/ExampleApplication.java
@@ -1,12 +1,18 @@
 package com.remcarpediem.netty.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExampleApplication {
+public class ExampleApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ExampleApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
     }
 }
