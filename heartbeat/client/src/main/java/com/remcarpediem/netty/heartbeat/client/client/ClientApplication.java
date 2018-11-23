186c0d430350905a3d8a32223c5c7f98e5db5fba
Randy <ztelur@gmail.com>
2018-11-23 10:27:09 +0800

    update

---
+++ heartbeat/client/src/main/java/com/remcarpediem/netty/heartbeat/client/client/ClientApplication.java
@@ -0,0 +1,12 @@
+package com.remcarpediem.netty.heartbeat.client.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+}
